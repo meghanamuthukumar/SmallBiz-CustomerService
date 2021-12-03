a16ed2c1a5c566caf4a208c788a4e5961d2ac19a
meghanamuthukumar <meghanamuthukumar14@gmail.com>
2021-12-03 16:14:01 +0530

    initial commit

---
+++ backend/src/main/java/com/example/smallbiz/SmallBizApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallbiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallBizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallBizApplication.class, args);
+	}
+
+}
